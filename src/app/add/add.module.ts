60a7cd55ed1c8d1312b0329bcc2fb41dfdd1eb9f
Jitto Thomas <jittotp@gmail.com>
2020-09-22 15:17:41 +0530

    imports added

--- src/app/add/add.module.ts
+++ src/app/add/add.module.ts
@@ -5,12 +5,13 @@ import { DoctorComponent } from './doctor/doctor.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material.module';
 import { AddRoutingModule } from './add-routing.module';
-import { MatFormField } from '@angular/material/form-field';
 import { CommonModule, DatePipe } from '@angular/common';
+import { MatSortModule } from '@angular/material/sort';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [PersonalComponent, ContactComponent, DoctorComponent],
-  imports: [AddRoutingModule, FormsModule, ReactiveFormsModule, MaterialModule, CommonModule],
+  imports: [AddRoutingModule, FormsModule, ReactiveFormsModule, MaterialModule, CommonModule, MatSortModule, BrowserAnimationsModule],
   providers: [DatePipe]
 })
 
