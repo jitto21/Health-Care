958e6376f049c3f4b013d54f58e4a4eebe37bff1
Jitto Thomas <jittotp@gmail.com>
2020-09-23 16:19:31 +0530

    implemented conditional filtering

--- src/app/show/show.component.ts
+++ src/app/show/show.component.ts
@@ -16,11 +16,25 @@ export class ShowComponent implements OnInit {
 
   editForm: FormGroup;
 
-  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
   editMode: boolean = false;
   forEditRows: any = [];
-  searchValue: string = null;
+  searchValue: string = '';
   isFilterChecked: boolean;
+  selected = "ALL";
+  matchSelect = new FormControl('ALL');
+  filterValues = {
+    'name': '',
+    'reg no:': '',
+    'gender': '',
+    'd.o.b': '',
+    'address': '',
+    'pincode': '',
+    'personal number': '',
+    'home/office number': '',
+    'department': '',
+    'doctor': ''
+  };
   public depts = ['Gastroenterology', 'Cardiology', 'Dental', 'Dermatology', 'Endocrinology', 'ENT', 'Diabetologist', 'General Medicine', 'General Medicine',
     'Nephrology', 'Neurology', 'Obstetrics & Gynecology', 'Oncology & Radiation Oncology', 'Ophthalmology', 'Orthopaedics', 'Pathology', 'Radiology', 'Urology'];
   public doctors = ['Dr. Benjamin Richards', 'Dr. Lewis Frank'];
@@ -51,6 +65,8 @@ export class ShowComponent implements OnInit {
       dept: new FormControl('', [Validators.required]),
       doctor: new FormControl('', [Validators.required])
     })
+
+    this.dataSource.filterPredicate = this.createFilterAll();
   }
 
   addresses(): FormArray {
@@ -59,18 +75,18 @@ export class ShowComponent implements OnInit {
 
   newAddress(address: string): FormGroup {
     return this.fb.group({
-     address: [address, [Validators.required]]
+      address: [address, [Validators.required]]
     })
   }
 
   addAddress(address: string) {
-    console.log("pushing: ",address)
-   this.addresses().push(this.newAddress(address));
+    console.log("pushing: ", address)
+    this.addresses().push(this.newAddress(address));
   }
 
   removeAddress(i: number) {
-   this.addresses().removeAt(i);
-   console.log("To remove: ",this.addresses().length);
+    this.addresses().removeAt(i);
+    console.log("To remove: ", this.addresses().length);
   }
 
   ngOnInit(): void {
@@ -88,26 +104,56 @@ export class ShowComponent implements OnInit {
         panelClass: panel
       });
     })
+
+    //subscribing to match select's value changes. Activates whenever the select is changed
+
+    this.matchSelect.valueChanges.subscribe(name => {
+      this.selected = name;
+      console.log(`%c Select Changed ${this.selected}`, 'color: brown')
+      Object.keys(this.filterValues).forEach(filterPpty => {
+        this.filterValues[filterPpty] = '';
+      }) // clearing all filter values
+
+      console.log("clearing all filter values ", this.filterValues);
+
+      if (name === 'ALL') { //assign each object in filterValues with searchValue
+        this.dataSource.filterPredicate = this.createFilterAll(); //assigning All filter
+        console.log("all selected")
+        Object.keys(this.filterValues).forEach(filterPpty => {
+          this.filterValues[filterPpty] = this.searchValue;
+        })
+      }
+
+      if (name !== 'ALL') {
+        this.dataSource.filterPredicate = this.createFilter(); //assigning different filter
+        console.log(this.dataSource.filterPredicate);
+        this.filterValues[name.toLocaleLowerCase()] = this.searchValue;
+      }
+      console.log("NEW filter values ", this.filterValues);
+      this.onFilterData(this.isFilterChecked) //filter the data when select is changed
+      // console.log(this.filterValues);
+      // this.dataSource.filter = JSON.stringify(this.filterValues);
+    })
     // this.transpose();
     // this.fillLabels();
   }
 
   fetchRecordsandJoinAddress() {
     this.records = this.recService.fetchRecords();
-  this.dataSource.data = this.records;
-  setTimeout(() => {
-    this.dataSource.sort = this.sort;
-  })
-  this.joinAddress();
+    this.dataSource.data = this.records;
+    setTimeout(() => {
+      this.dataSource.sort = this.sort;
+    })
+    this.joinAddress();
   }
 
   joinAddress() {
     this.records.forEach(record => { //each record
       let joinedAdrs = '';
       record.addresses.forEach((address, index) => { //each address eg: address 1 address 2 and so on..
-        if(index!=0)
+        if (index != 0)
           joinedAdrs += ', ';
-        joinedAdrs+= address.address;
+        joinedAdrs += address.address;
       })
       record['joinedAdrs'] = joinedAdrs
       console.log(joinedAdrs);
@@ -117,6 +163,10 @@ export class ShowComponent implements OnInit {
   onEdit(record) {
     console.log(record);
     this.editMode = true;
+    this.searchValue = '';
+    Object.keys(this.filterValues).forEach(filterPpty => {
+      this.filterValues[filterPpty] = '';
+    })
     this.forEditRows = [record];
     let index = this.forEditRows.indexOf(record);
     console.log("INDEX: ", index);
@@ -149,7 +199,7 @@ export class ShowComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      if(result=== 'yes') {
+      if (result === 'yes') {
         this.editMode = false;
         this.forEditRows = [];
       }
@@ -157,15 +207,98 @@ export class ShowComponent implements OnInit {
   }
 
   search(value: string) {
-    this.dataSource.filter = '';
-    this.isFilterChecked = false;
+    // this.dataSource.filter = '';
+    // this.isFilterChecked = false;
     this.searchValue = value.trim().toLocaleLowerCase();
     console.log("Search value: ", this.searchValue);
+    if (this.selected !== 'ALL') { this.filterValues[this.selected.toLocaleLowerCase()] = this.searchValue; }
+    console.log(this.filterValues);
+    if (this.selected === 'ALL') { //assign each object in filterValues with searchValue
+      console.log("all selected, typing in search box")
+      Object.keys(this.filterValues).forEach(filterPpty => {
+        this.filterValues[filterPpty] = this.searchValue;
+      })
+    }
+    if (this.isFilterChecked) { this.onFilterData(this.isFilterChecked); }
+    // this.dataSource.filter = JSON.stringify(this.filterValues);
   }
 
   onFilterData(checked: boolean) {
-    console.log("Checking: ", checked);
-    this.dataSource.filter = checked ? this.searchValue : null;
+    console.log("checked arg value: ", checked);
+    this.isFilterChecked = checked;
+    // if(this.selected === 'ALL') {
+    //   console.log("filtering: ALL");
+    //   return this.dataSource.filter = checked ? this.searchValue : null;
+    // }
+    // console.log(`filtering: ${this.selected}`);
+    // this.filterValues[this.selected.toLocaleLowerCase()] = this.selected
+    console.log(JSON.stringify(this.filterValues));
+    this.dataSource.filter = this.isFilterChecked ? JSON.stringify(this.filterValues) : '';
+  }
+
+  createFilter(): (data: any, filter: string) => boolean {
+    let filterFunction = function (data, filter): boolean {
+      let searchTerms = JSON.parse(filter);
+      console.log("createFilter");
+      console.log("************");
+      console.log(data);
+      console.log(searchTerms);
+      console.log(`%c ${data.name}`, 'color: yellow')
+      console.log(`${data['name']}: `, data['name'].toLowerCase().indexOf(searchTerms['name']) !== -1)
+      console.log(`${data['regno']}: `, data['regno'].toString().toLowerCase().indexOf(searchTerms['reg no:']) !== -1)
+      console.log(`${data['gender']}: `, data['gender'].toLowerCase().indexOf(searchTerms['gender']) !== -1)
+      console.log(`${data['dob']}: `, data['dob'].toLowerCase().indexOf(searchTerms['d.o.b']) !== -1)
+      console.log(`${data['joinedAdrs']}: `, data['joinedAdrs'].toLowerCase().indexOf(searchTerms['address']) !== -1)
+      console.log(`${data['pincode']}: `, data['pincode'].toString().toLowerCase().indexOf(searchTerms['pincode']) !== -1)
+      console.log(`${data['pmobile']}: `, data['pmobile'].toString().toLowerCase().indexOf(searchTerms['personal number']) !== -1)
+      console.log(`${data['hmobile']}: `, data['hmobile'].toString().toLowerCase().indexOf(searchTerms['home/office number']) !== -1)
+      console.log(`${data['doctor']}: `, data['doctor'].toLowerCase().indexOf(searchTerms['doctor']) !== -1);
+
+      return data['name'].toLowerCase().indexOf(searchTerms['name']) !== -1
+        && data['regno'].toString().toLowerCase().indexOf(searchTerms['reg no:']) !== -1
+        && data['gender'].toLowerCase().indexOf(searchTerms['gender']) !== -1
+        && data['dob'].toLowerCase().indexOf(searchTerms['d.o.b']) !== -1
+        && data['joinedAdrs'].toLowerCase().indexOf(searchTerms['address']) !== -1
+        && data['pincode'].toString().toLowerCase().indexOf(searchTerms['pincode']) !== -1
+        && data['pmobile'].toString().toLowerCase().indexOf(searchTerms['personal number']) !== -1
+        && data['hmobile'].toString().toLowerCase().indexOf(searchTerms['home/office number']) !== -1
+        && data['dept'].toLowerCase().indexOf(searchTerms['department']) !== -1
+        && data['doctor'].toLowerCase().indexOf(searchTerms['doctor']) !== -1
+    }
+    return filterFunction;
+  }
+
+  createFilterAll(): (data: any, filter: string) => boolean {
+    let filterFunction = function (data, filter): boolean {
+      let searchTerms = JSON.parse(filter);
+      console.log("createFilterAll");
+      console.log("***************")
+      console.log(data);
+      console.log(searchTerms);
+      console.log(`%c ${data.name}`, 'color: yellow')
+      console.log(`${data['name']}: `, data['name'].toLowerCase().indexOf(searchTerms['name']) !== -1)
+      console.log(`${data['regno']}: `, data['regno'].toString().toLowerCase().indexOf(searchTerms['reg no:']) !== -1)
+      console.log(`${data['gender']}: `, data['gender'].toLowerCase().indexOf(searchTerms['gender']) !== -1)
+      console.log(`${data['dob']}: `, data['dob'].toLowerCase().indexOf(searchTerms['d.o.b']) !== -1)
+      console.log(`${data['joinedAdrs']}: `, data['joinedAdrs'].toLowerCase().indexOf(searchTerms['address']) !== -1)
+      console.log(`${data['pincode']}: `, data['pincode'].toString().toLowerCase().indexOf(searchTerms['pincode']) !== -1)
+      console.log(`${data['pmobile']}: `, data['pmobile'].toString().toLowerCase().indexOf(searchTerms['personal number']) !== -1)
+      console.log(`${data['hmobile']}: `, data['hmobile'].toString().toLowerCase().indexOf(searchTerms['home/office number']) !== -1)
+      console.log(`${data['doctor']}: `, data['doctor'].toLowerCase().indexOf(searchTerms['doctor']) !== -1);
+
+      return data['name'].toLowerCase().indexOf(searchTerms['name']) !== -1
+        || data['regno'].toString().toLowerCase().indexOf(searchTerms['reg no:']) !== -1
+        || data['gender'].toLowerCase().indexOf(searchTerms['gender']) !== -1
+        || data['dob'].toLowerCase().indexOf(searchTerms['d.o.b']) !== -1
+        || data['joinedAdrs'].toLowerCase().indexOf(searchTerms['address']) !== -1
+        || data['pincode'].toString().toLowerCase().indexOf(searchTerms['pincode']) !== -1
+        || data['pmobile'].toString().toLowerCase().indexOf(searchTerms['personal number']) !== -1
+        || data['hmobile'].toString().toLowerCase().indexOf(searchTerms['home/office number']) !== -1
+        || data['dept'].toLowerCase().indexOf(searchTerms['department']) !== -1
+        || data['doctor'].toLowerCase().indexOf(searchTerms['doctor']) !== -1;
+
+    }
+    return filterFunction;
   }
 
   // transpose() {
